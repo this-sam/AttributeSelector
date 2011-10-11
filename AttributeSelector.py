1e7ad7b6083db6d5b10157a545534c0461dc329d
Samuel Bennett Brown <sbbrown@wapiti.uvm.edu>
2011-10-11 13:52:55 -0400

    Initial Commit

---
+++ AttributeSelector.py
@@ -0,0 +1,23 @@
+class AttributeSelector:
+	
+	import os
+	
+	#----------File Location Constants---------
+	CHAT_DIR = 'chats'
+	QUES_DIR = 'questionnaires'
+	
+	def __init__(self):
+		#get input files
+		#store files in dictionary --> USERNAME => 
+		
+		#get questionnaires
+		#store files in dictionary --> USERNAME => Questionnaire
+		
+		pass
+
+	def __get_files(self):
+		pass
+
+
+if __name__ == '__main__':
+    selector = AttributeSelector()
---
+++ README.txt
@@ -0,0 +1,5 @@
+This is the software that I will use to prepare vectors of attributes
+from chat conversations and write them to a file readable by various
+data mining algorithms.
+
+By Sam Brown
